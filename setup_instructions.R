d6b021369440ced5dc76520a5feb7c888ee889d1
Pedro Henrique Pereira Braga <ph.pereirabraga@gmail.com>
2021-05-20 12:30:28 -0400

    Adjusting .gitignore and setting instructions up

---
+++ setup_instructions.R
@@ -0,0 +1,19 @@
+# Setup script
+
+remotes::install_github("rstudio/blogdown")
+
+# usethis::create_project()
+
+# blogdown::new_site(theme = "themefisher/airspace-hugo")
+
+blogdown::serve_site()
+
+blogdown::new_post(title = "Hi Hugo", 
+                   ext = '.Rmarkdown', 
+                   subdir = "post")
+
+# usethis::use_git()
+
+# usethis::use_github() # requires a GitHub PAT
+
+# usethis::use_github_action("blogdown2")
